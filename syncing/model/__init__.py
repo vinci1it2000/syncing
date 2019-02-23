835e22e778d03e990587eeafd1effac92d412dfa
vincenzoarcidiacono <vinci1it2000@gmail.com>
2019-02-23 15:08:26 +0100

    feat(model): Add model.

---
+++ syncing/model/__init__.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+#
+# Copyright 2019 European Commission (JRC);
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+"""
+It contains utility functions and the computational model `dsp` to synchronise
+and re-sample data-sets.
+
+Sub-Modules:
+
+.. currentmodule:: syncing.model
+
+.. autosummary::
+    :nosignatures:
+    :toctree: model/
+
+    interp
+"""
+import schedula as sh
+
+dsp = sh.BlueDispatcher(name='Computational Model', raises=True)
+
+
+@sh.add_function(dsp, outputs=['labels'], inputs_kwargs=True,
+                 inputs_defaults=True)
+def define_labels(x_label='x', y_label='y'):
+    """
+    Defines `reference-labels` (i.e., "x", "y") for each data-set.
+
+    :param x_label:
+        Default `var-name` of the common x-axis to synchronise and re-sampled
+        the data-sets.
+    :type x_label: str
+
+    :param y_label:
+        Default `var-name` of the common y-axis to synchronise the data-sets.
+    :type y_label: str
+
+    :return:
+        Reference-labels (i.e., "x", "y") for each data-set.
+
+        It is like `{"<set-name>": {"x": "<x-label>", "y": "<y-label>"}, ...}`.
+    :rtype: collections.defaultdict
+    """
+    import collections
+    return collections.defaultdict(lambda: dict(x=x_label, y=y_label))
+
+
+def _get(labels, name, data, *keys):
+    labels = labels[name]
+    return [data[labels[key]] for key in keys]
+
+
+def _compute_shifts(ref, *data):
+    import numpy as np
+    import numpy.fft as fft
+
+    l, h = zip(*((x.min(), x.max()) for x, y in (ref,) + data))
+    l, h = min(l), max(h)
+
+    dx = float(np.median(np.diff(ref[0])) / 10)
+
+    X = np.arange(l, h + dx, dx)
+    Y = np.interp(X, *ref)
+
+    zero_index, f1 = int(len(Y) / 2) - 1, fft.fft(Y)
+    for x, y in data:
+        f2 = fft.fft(np.flipud(np.interp(X, x, y)))
+        c = fft.fftshift(np.real(fft.ifft(f1 * f2)))
+        assert len(c) == len(Y)
+        yield float((zero_index - np.argmax(c)) * dx)
+
+
+@sh.add_function(dsp, outputs=['shifts'])
+def calculate_shifts(labels, reference_name, data):
+    """
+    Calculates the shifts from the reference data-set.
+
+    :param labels:
+        Reference-labels (i.e., "x", "y") for each data-set.
+
+        It is like `{"<set-name>": {"x": "<x-label>", "y": "<y-label>"}, ...}`.
+    :type labels: collections.defaultdict
+
+    :param reference_name:
+        Reference data-set name.
+    :type reference_name: str
+
+    :param data:
+        Data-sets.
+    :type data: dict[str, dict[str, numpy.array]]
+
+    :return:
+        Shifts from the reference data-set.
+    :rtype: dict[str, float]
+    """
+    data = {k: _get(labels, k, v, 'x', 'y') for k, v in data.items()}
+    keys = [k for k in data if k != reference_name]
+    args = sh.selector([reference_name] + keys, data, output_type='list')
+    return sh.map_list(keys, *_compute_shifts(*args))
+
+
+def _interpolate(x, x_label, data, methods):
+    xp = data[x_label]
+    return {
+        k: methods[k](x, xp=xp, fp=v) for k, v in data.items() if k != x_label
+    }
+
+
+@sh.add_function(dsp, outputs=['methods'], inputs_kwargs=True,
+                 inputs_defaults=True, weight=sh.inf(1, 0))
+def define_interpolation_methods(interpolation_method='linear'):
+    """
+    Defines interpolation methods for each variable of each data-set.
+
+    :param interpolation_method:
+        Default interpolation method.
+    :type interpolation_method: str
+
+    :return:
+        Interpolation methods for each variable of each data-set.
+
+        It is like `{"<set-name>": {"<var-name>": "<interp>", ...}, ...}`.
+    :rtype: collections.defaultdict
+    """
+    import collections
+    from .interp import METHODS
+    method = METHODS[interpolation_method]
+    default = collections.defaultdict(lambda: method)
+    return collections.defaultdict(lambda: default)
+
+
+@sh.add_function(dsp, outputs=['resampled'])
+def resample_data(labels, reference_name, data, shifts, methods):
+    """
+    Resample all data-sets using the reference signal.
+
+    :param labels:
+        Reference-labels (i.e., "x", "y") for each data-set.
+
+        It is like `{"<set-name>": {"x": "<x-label>", "y": "<y-label>"}, ...}`.
+    :type labels: collections.defaultdict
+
+    :param reference_name:
+        Reference data-set name.
+    :type reference_name: str
+
+    :param data:
+        Data-sets.
+    :type data: dict[str, dict[str, numpy.array]]
+
+    :param shifts:
+        Shifts from the reference data-set.
+    :type shifts: dict[str, float]
+
+    :param methods:
+        Interpolation methods for each variable of each data-set.
+
+        It is like `{"<set-name>": {"<var-name>": "<interp>", ...}, ...}`.
+    :type methods: collections.defaultdict
+
+    :return:
+        Resampled data-sets.
+    :rtype: dict[str, dict[str, numpy.array]]
+    """
+    x = data[reference_name][labels[reference_name]['x']]
+    r, res = {reference_name: data[reference_name]}, {}
+    for k, s in shifts.items():
+        r[k] = _interpolate(x + s, labels[k]['x'], data[k], methods[k])
+
+    for (i, j), v in sh.stack_nested_keys(r):
+        j = sh.stlp(j)
+        sh.get_nested_dicts(res, i, *j[:-1])[j[-1]] = v
+
+    return res
---
+++ syncing/model/interp.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+#
+# Copyright 2019 European Commission (JRC);
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+"""
+It contains utility functions and methods to interpolate the data-sets.
+"""
+import functools
+import numpy as np
+import scipy.interpolate as sci_itp
+
+
+def _interp_wrapper(func, x, xp, fp, **kw):
+    if isinstance(kw.get('fill_value'), tuple) and not kw['fill_value']:
+        kw['fill_value'] = fp[0], fp[-1]
+    return np.nan_to_num(func(xp, fp, **kw)(x))
+
+
+def polynomial_interpolation(x, xp, fp, order=1):
+    """
+    Re-samples data using the polynomial interpolation.
+
+    :param x:
+        The x-coordinates of the re-sampled values.
+    :type x: numpy.array
+
+    :param xp:
+        The x-coordinates of the data points.
+    :type xp: numpy.array
+
+    :param fp:
+        The y-coordinates of the data points, same length as xp.
+    :type fp: numpy.array
+
+    :return:
+        Re-sampled y-values.
+    :rtype: numpy.array
+    """
+    return np.poly1d(np.polyfit(xp, fp, order))(x)
+
+
+def _cum_integral(x, xp, fp):
+    from scipy.integrate import cumtrapz
+    X = np.unique(np.concatenate((x, xp)))
+    Y = np.interp(X, xp, fp, left=0.0, right=0.0)
+    return cumtrapz(Y, X, initial=0)[np.searchsorted(X, x)]
+
+
+def integral_interpolation(x, xp, fp):
+    """
+    Re-samples data maintaining the signal integral.
+
+    :param x:
+        The x-coordinates of the re-sampled values.
+    :type x: numpy.array
+
+    :param xp:
+        The x-coordinates of the data points.
+    :type xp: numpy.array
+
+    :param fp:
+        The y-coordinates of the data points, same length as xp.
+    :type fp: numpy.array
+
+    :return:
+        Re-sampled y-values.
+    :rtype: numpy.array
+    """
+
+    x, fp = np.asarray(x, dtype=float), np.asarray(fp, dtype=float)
+    xp = np.asarray(xp, dtype=float)
+    n = len(x)
+    X, dx = np.zeros(n + 1), np.zeros(n + 1)
+    dx[1:-1] = np.diff(x)
+    X[0], X[1:-1], X[-1] = x[0], x[:-1] + dx[1:-1] / 2, x[-1]
+    I = np.diff(_cum_integral(X, xp, fp))
+
+    dx /= 8.0
+    # noinspection PyTypeChecker
+    A = np.diag((dx[:-1] + dx[1:]) * 3.0)
+    i, j = np.indices(A.shape)
+    A[i == j - 1] = A[i - 1 == j] = dx[1:-1]
+
+    return np.linalg.solve(A, I)
+
+
+METHODS = ('linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic')
+_kw = dict(fill_value=(), copy=False, bounds_error=False)
+METHODS = {
+    k: functools.partial(_interp_wrapper, sci_itp.interp1d, kind=k, **_kw)
+    for k in METHODS
+}
+METHODS['pchip'] = functools.partial(
+    _interp_wrapper, sci_itp.PchipInterpolator
+)
+METHODS['akima'] = functools.partial(
+    _interp_wrapper, sci_itp.Akima1DInterpolator
+)
+METHODS['integral'] = integral_interpolation
+for i in range(5):
+    METHODS['polynomial%d' % i] = functools.partial(
+        polynomial_interpolation, order=i
+    )
+
+for i in range(5, 10, 2):
+    METHODS['spline%d' % i] = functools.partial(
+        _interp_wrapper, sci_itp.interp1d, kind=i, **_kw
+    )
